b1e7e0a62f28a3756bef2b716755fd688aed935e
Salt_For_Sale <elee823@gmail.com>
2024-04-16 14:58:43 -0400

    broken bg and nav stuff uhhhh yeah

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var currentTime = new Date().getSeconds();
+  if (currentTime % 2 == 0) {
+    document.body.classList.add('start-bg');
+  }
+  else {
+    document.body.classList.add('start-bg-day');
+  }
+});
+
 var chain = $("#chains")[0];
 $(".img-wrapper").mouseenter(function() {
     chain.currentTime = 0;
