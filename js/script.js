f21b07bfdb69973563061ecc053850cc342e3be2
Salt_For_Sale <elee823@gmail.com>
2024-05-28 13:46:48 -0400

    cleanup

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var currentTime = new Date().getSeconds();
-  if (currentTime % 2 == 0) {
-    document.body.classList.add('start-bg');
-  }
-  else {
-    document.body.classList.add('start-bg-day');
-  }
-});
-
-
 var chain = $("#chains")[0];
 $(".img-wrapper").mouseenter(function () {
   chain.currentTime = 0;
   chain.play();
 });
+
+
+const vue_app = Vue.createApp({
+  // This automatically imports your movies.json file and puts it into
+  //   the variable: movies
+  created() {
+        fetch('projects.json').then(response => response.json()).then(json => {
+              this.projects = json
+        })
+  },
+  data() {
+        return {
+              // This holds your movies.json data.
+              projects: [],
+              /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+        }
+  },
+  method() {
+    
+  }
+})
+
+vue_app.mount("#vue_app")
